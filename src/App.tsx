c2974d5c861c4d4a1c8db41f7d4dca1a71eb41e8
AdamsEugene <adamseugene292@gmail.com>
2022-12-29 21:42:58 +0000

    test deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
-import { useCounter } from './hooks/useCounter'
-import './App.css'
+import { useCounter } from "./hooks/useCounter";
+import "./App.css";
 
 function App() {
-  const { count, increment } = useCounter()
+  const { count, increment } = useCounter();
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hello Vite + React!</p>
+        <p>Hello Vite + Reacts!</p>
         <p>
-          <button
-            type="button"
-            onClick={increment}
-          >
+          <button type="button" onClick={increment}>
             count is: {count}
           </button>
         </p>
@@ -28,7 +25,7 @@ function App() {
           >
             Learn React
           </a>
-          {' | '}
+          {" | "}
           <a
             className="App-link"
             href="https://vitejs.dev/guide/features.html"
@@ -40,7 +37,7 @@ function App() {
         </p>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
