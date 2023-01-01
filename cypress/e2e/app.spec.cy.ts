850467e12447d9e4fffcfac21f06151fd5bb4f15
AdamsEugene <adamseugene292@gmail.com>
2023-01-01 23:50:20 +0000

    fix: cypress

--- cypress/e2e/app.spec.cy.ts
+++ cypress/e2e/app.spec.cy.ts
@@ -1,3 +1,5 @@
+import {} from 'cypress';
+
 describe('empty spec', () => {
 	beforeEach(() => {
 		cy.visit('http://127.0.0.1:5173/');
@@ -5,5 +7,9 @@ describe('empty spec', () => {
 
 	it('should increase count when clicked', () => {
 		cy.get('button').click().should('have.text', 'count is: 1');
+	});
+
+	it('Should change the theme', () => {
+		cy.get('div').click();
 	});
 });
